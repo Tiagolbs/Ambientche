600722ab9174d16ba538525ff0f9a157ff79e22c
Tiago Seben <tiagolbs31@gmail.com>
2019-11-12 19:57:11 -0300

    Sistema de login no postgresql e status da luz

--- Python/pySQL.py
+++ Python/pySQL.py
@@ -3,13 +3,18 @@ import _thread
 import time
 import psycopg2
 from datetime import datetime
+from getpass import getpass
 
-HOST = ''              # Endereco IP do Servidor
+HOST = '192.168.9.19'              # Endereco IP do Servidor
 PORT = 5001            # Porta que o Servidor esta
 
-conSql = psycopg2.connect(user = "postgres",password = "password",host = "localhost",port = "5433",database = "LogTest")
+conSql = psycopg2.connect(user = input('User postgres: '),password = getpass('Senha: '),host = input('Host: '),port = input('Porta: '),database = input('Database: '))
 
 cur = conSql.cursor()
+
+print('Conectado com sucesso')
+
+StatusLuz = [False, False, False, False, False, False, False]
 
 def conectado(con, cliente):
     print ('Conectado por', cliente)
@@ -17,12 +22,14 @@ def conectado(con, cliente):
     while True:
         msg = con.recv(1024)
         if int(msg) >= 2 and int(msg) <= 8:
-            sql = "INSERT INTO LogTest1 (dt_string, comodo) VALUES (%s, %s)"
+            sql = "INSERT INTO LogTest1 (dt_string, comodo, status) VALUES (%s, %s, %s)"
             now = datetime.now()
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            val = (dt_string, int(msg))
+            StatusLuz[int(msg)-2] = not StatusLuz[int(msg)-2]
+            val = (dt_string, int(msg), StatusLuz[int(msg)-2])
             cur.execute(sql, val)
             conSql.commit()
+            print('Inserido:', dt_string, int(msg), StatusLuz[int(msg)-2])
 
     con.close()
     _thread.exit()
