33949114e513835f6a3573f0788e5ec684a60f38
ricardofachinello <ricardomatheu@hotmail.com>
2019-11-20 00:30:38 +0000

    Add files via upload

--- Java/anything.java
+++ Java/anything.java
@@ -1,6 +1,21 @@
-class Foo {
-    public static void main(String[] args) {
-       String $h͉̠͇̹̖̼̖͈̬̘͕͎͈̬̺̺̯̅̃ͯ̈̂̐̿̍̌͑ͩ̑ͦ̿͡e̛̛̹̞͚̭̻͇̰̟͙̱͉̣̼̻̝͕̭ͯͦ͌̒̽̐l̸̵̷̛͍̬͎̲̲̹̙͙̞͚̲̭̫̬ͭ͗̒ͯ̈́ͣ͂ͧ̌̿ͮ̈́ͫ̽ͩ̈͞ṗ̨̟̰͈̹̗͖̰̙͉͎̗͓̬̳ͤ͒ͣ̓̂͛͑̑ͦͮ̅͆̈ͥ̈́́̈̋́̕͞ = "Ambientche"; 
-       System.out.println($h͉̠͇̹̖̼̖͈̬̘͕͎͈̬̺̺̯̅̃ͯ̈̂̐̿̍̌͑ͩ̑ͦ̿͡e̛̛̹̞͚̭̻͇̰̟͙̱͉̣̼̻̝͕̭ͯͦ͌̒̽̐l̸̵̷̛͍̬͎̲̲̹̙͙̞͚̲̭̫̬ͭ͗̒ͯ̈́ͣ͂ͧ̌̿ͮ̈́ͫ̽ͩ̈͞ṗ̨̟̰͈̹̗͖̰̙͉͎̗͓̬̳ͤ͒ͣ̓̂͛͑̑ͦͮ̅͆̈ͥ̈́́̈̋́̕͞);
+import java.io.*; 
+import java.net.*; 
+  
+class pinga { 
+  // Sends ping request to a provided IP address 
+  public static void sendPingRequest(String ipAddress) throws UnknownHostException, IOException { 
+    InetAddress main = InetAddress.getByName(ipAddress); 
+    if (main.isReachable(5000)){
+      System.out.println("Feito"); 
+    } 
+    else{
+      System.out.println("Deu Ruim"); 
     }
- } 
+  } 
+  
+  // Driver code 
+  public static void main(String[] args) throws UnknownHostException, IOException { 
+    String ipAddress = "192.168.9.20"; 
+    sendPingRequest(ipAddress); 
+  } 
+} 
